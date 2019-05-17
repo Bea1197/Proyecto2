4f8f51f70778d6a590f222964a6f419b55b84e99
Bea1197 <beatrizleiva415@gmail.com>
2019-05-16 22:18:07 -0600

    Proyecto 2 inicio

---
+++ src/proyecto2_b53820_b64816/Proyecto2_b53820_b64816.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2_b53820_b64816;
+
+import GUI.Window;
+
+/**
+ *
+ * @author beatriz
+ */
+public class Proyecto2_b53820_b64816 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Window w = new Window();
+      w.setVisible(true);
+      w.setLocationRelativeTo(null);
+    }
+    
+}
